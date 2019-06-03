1e2dabb1233176a4b54318dea86400a47a623f61
mingmin0101 <mingmin0101@gmail.com>
2019-06-03 15:10:52 +0800

    maggie's new map.php

--- map.php
+++ map.php
@@ -2,9 +2,9 @@
 session_start();
 include("pdoInc.php");     //PDO
 ?>
-
-<!-- 
-https://leafletjs.com/examples/quick-start/ 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--
+https://leafletjs.com/examples/quick-start/
 https://leafletjs.com/reference-1.4.0.html#map-example
 -->
 <html>
@@ -19,8 +19,8 @@ https://leafletjs.com/reference-1.4.0.html#map-example
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-
-
+    <!-- icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <!-- leaflet map api -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css"
       integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
@@ -48,12 +48,12 @@ https://leafletjs.com/reference-1.4.0.html#map-example
     /* Hide the carousel text when the screen is less than 600 pixels wide */
     @media (max-width: 600px) {
       .carousel-caption {
-        display: none; 
+        display: none;
       }
     }
 
     </style>
-  
+
     <!-- 聊天室相關 -->
     <style>
         table,tr,td{width:100%; font-family: 微軟正黑體; font-size: 15px;}
@@ -71,7 +71,7 @@ https://leafletjs.com/reference-1.4.0.html#map-example
         );
         $("#msg").val("");
     }
-     
+
     function showMsg(t){
         $.post(
             "ajax_chatroom_disp.php",
@@ -83,7 +83,7 @@ https://leafletjs.com/reference-1.4.0.html#map-example
             setTimeout("showMsg(1);", 1000);
         });
     }
-     
+
     $(function(){
         // 網頁載入，抓取全部訊息
         showMsg(0);
@@ -98,7 +98,7 @@ https://leafletjs.com/reference-1.4.0.html#map-example
     })
     </script>
 
-    
+
 
 </head>
 <body style="font-family: 微軟正黑體;">
@@ -119,23 +119,23 @@ https://leafletjs.com/reference-1.4.0.html#map-example
             <li class="nav-item">
               <a class="nav-link" href="#recommendation">隨機推薦</a>
             </li>
-            
+
         </ul> -->
         <ul class="navbar-nav ml-auto" >
             <?php
-                if(isset($_SESSION['account']) && $_SESSION['account'] != null){ 
+                if(isset($_SESSION['account']) && $_SESSION['account'] != null){
                     //修改會員資料
                     // echo '<a href="modify_member_info.php"><img src="hw6_pic/profile.png" style="position: fixed; right: 140px; top:95px; height:32px;" onmouseover="this.src=\'hw6_pic/profile_hover.png\'" onmouseleave="this.src=\'hw6_pic/profile.png\'"></a>';
                     //會員登出
                     echo '<li class="nav-item"><a href="php_sess_logout.php"><img src="pic/logout.png" style="height:32px;" onmouseover="this.src=\'pic/logout_hover.png\'" onmouseleave="this.src=\'pic/logout.png\'" onclick="<?php echo \'<meta http-equiv=REFRESH CONTENT=0;url=index.php>\';?>"></a></li>';
-                } 
+                }
                 else{
                     echo '<li class="nav-item"><a href="member.php"><img src="pic/login.png" style="height:32px;" onmouseover="this.src=\'pic/login_hover.png\'" onmouseleave="this.src=\'pic/login.png\'"></a></li>';
                 }
             ?>
             <!-- <li class="nav-item">
               <a class="nav-link" href="member.php"><img src="pic/login.png" style="height:32px;" onmouseover="this.src='pic/login_hover.png'" onmouseleave="this.src='pic/login.png'"></a>
-            </li>   -->  
+            </li>   -->
         </ul>
     </nav>
 
@@ -143,7 +143,7 @@ https://leafletjs.com/reference-1.4.0.html#map-example
 
 
     <div class="container-fluid">
-      
+
     <div id='mapView' class="row">
       <div class="col-sm-8" style="padding: 5px">
         <!-- map -->
@@ -153,70 +153,96 @@ https://leafletjs.com/reference-1.4.0.html#map-example
 
       <div class="col-sm-4" style="padding: 5px">
         <br><br><br>
-        <!-- 到時候要用php從資料庫撈取餐廳資料 -->
-        <div id='restaurantInto'  style="height: 500px; width: 100%; overflow-y:scroll;">
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-            <div>
-            <h3 >餐廳</h3>
-            <p>這裡是餐廳資料</p>
-            </div>
-            <hr>
-
-        </div>
-
-      </div>
-    </div>
-
-    </div>
 
 
-<!-- 下面是map -->
+
+
+<!-- 下面是map======================================================================= -->
     <script type="text/javascript">
+        // https://noob.tw/openstreetmap/
+        //利用leaflet初始化地圖
         var map = L.map('mapid').setView([24.987567, 121.576072], 25);  //setView(經緯度中心點，zoom大小)
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        L.marker([24.987567, 121.576072]).addTo(map)
-            .bindPopup('政治大學校碑')
+  </script>
+  <!-- https://ithelp.ithome.com.tw/articles/10208985 寫php傳值給javascript參考-->
+  <?php
+        // 用php從資料庫撈取餐廳資料
+        $sql1 = "SELECT * FROM restaurant";
+        $result1 = mysqli_query($con,$sql1);
+        $token = array();
+        $k=0;
+        while($row1 = mysqli_fetch_row($result1))
+        {
+          $name[$k] = $row1[1];
+          $category[$k] = $row1[2];
+          $longitude[$k] = $row1[3];
+          $latitude[$k] = $row1[4];
+          $address[$k] = $row1[5];
+          $grade[$k] = $row1[6];
+          $open_hour[$k] = $row1[8];
+          $k++;
+        }
+        // print_r($name); //印出array測試看看
+  ?>
+
+  <script>
+        // 抓到的資料，轉成javascript用於map
+       var rest_name= <?php echo json_encode($name); ?>;
+       var category = <?php echo json_encode($category); ?>;
+       var longitude = <?php echo json_encode($longitude); ?>;
+       var latitude = <?php echo json_encode($latitude); ?>;
+       var address = <?php echo json_encode($address); ?>;
+       var grade = <?php echo json_encode($grade); ?>;
+       var open_hour = <?php echo json_encode($open_hour); ?>;
+
+       for(var i=0; i<rest_name.length ;i++){
+        L.marker([longitude[i], latitude[i]]).addTo(map)
+            .bindPopup('<strong>'+rest_name[i]+'</strong><br>'+ category[i]+'<br>'+address[i]+'<br>'+open_hour[i])
             .openPopup()
-            .on('click', onClick);
-           
+            // .on('click', onClick);
+        }
+
         function onClick(e){
             alert(this.getLatLng());
             document.getElementBiId('intro').value = '餐廳簡介在這裡';
         }
+
     </script>
 
-<!-- 聊天視窗 -->
+<!-- 下面是右邊餐廳簡介======================================================================= -->
+
+<div id='restaurantInto'  style="height: 500px; width: 100%; overflow-y:scroll;">
+<h3 align=center style="background-color:#0A5D90; color:#FFFFFF">政大美食地圖</h3>
+<div class="overflow-hidden">
+</div>
+<script>
+  // 由 for 迴圈來產生右邊餐廳簡介
+  for (i=0; i<rest_name.length; i++) {
+  	document.write("<a href=><font color=#0A5D90 size=5px>" + rest_name[i] + "</font></a><br>");
+    // document.write("<p style=background-color:#0A5D90; color:#FFFFFF;>政大美食地圖</p>");
+    document.write("<mark><img src=pic/star.png></img><font color=red size=3px background=pink>" + grade[i]+ "</font></mark><br>");
+    document.write("<font size=3px>今日營業: " + open_hour[i] + "</font><br>");
+    document.write("<font size=3px>" + address[i] + "</font><br><hr>");
+  }
+</script>
+    <!-- <div>
+    <h3 >餐廳</h3>
+    <p>這裡是餐廳資料</p>
+    </div>
+    <hr> -->
+</div>
+</div>
+</div>
+</div>
+
+<!-- 聊天視窗=================================================================================== -->
 <!-- <img id='chat_icon' src='pic/chat_icon.png' style="height: 60px; width: 60px; position: fixed; bottom: 30px; right: 35px;" onclick="this.style.display='none';getElementById('close_icon').style.display='block';getElementById('chatroom').style.display='block';" >
 <img id='close_icon' src='pic/close.png' style="height: 70px; width: 70px; position: fixed; bottom: 25px; right: 30px; display: none;" onclick="this.style.display='none';getElementById('chat_icon').style.display='block';getElementById('chatroom').style.display='none';" > -->
-  
+
 <div id='chatroom' class="container" style="position: fixed; right:30px; bottom: 100px; height:400px; width: 400px; display: none;">
     <div class="card">
         <div class="card-header bg-dark text-white">聊天室</div>
@@ -231,7 +257,7 @@ https://leafletjs.com/reference-1.4.0.html#map-example
                     </form>
                 </td></tr>
             </table>
-        </div> 
+        </div>
     </div>
 </div>
 
--- pdoInc.php
+++ pdoInc.php
@@ -1,9 +1,9 @@
 ﻿<?php
 //架站用來連接MYSQL
 // $db_server = "localhost";
-// $db_user = "id9473568_mingmin0101_2019web_hw6";
+// $db_user = "id9504225_foodnopoly";
 // $db_passwd = "web105306";
-// $db_name = "id9473568_2019web_hw6";
+// $db_name = "id9504225_web_project";
 
 //本機用來連接MYSQL
 $db_server = "localhost"; //本機
@@ -15,6 +15,9 @@ $db_name  = "web_project";    //Database的名字
 try {
     $dsn = "mysql:host=$db_server;dbname=$db_name";
     $dbh = new PDO($dsn, $db_user, $db_passwd);
+    //for孫和謝
+    $con=mysqli_connect($db_server, $db_user, $db_passwd,$db_name);
+    mysqli_query($con,"SET CHARACTER SET UTF8");//印中文
 }
 catch (Exception $e){
     die('無法對資料庫連線');
